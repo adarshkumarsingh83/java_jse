98ea730464c06150c501d9e8125694ec93d69563
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-09-29 13:37:54 -0500

    roman-numbers-generator

--- APPLICATION/UTILS/roman-numbers-generator/src/main/java/com/espark/adarsh/ApplicationMain.java
+++ APPLICATION/UTILS/roman-numbers-generator/src/main/java/com/espark/adarsh/ApplicationMain.java
@@ -3,7 +3,7 @@ package com.espark.adarsh;
 
 import com.espark.adarsh.service.RomanNumberGenerator;
 
-public class Main {
+public class ApplicationMain {
     public static void main(String[] args) {
         for (int i = 1; i <= 3999; i++) {
             System.out.println("i=" + i + " -> " + RomanNumberGenerator.romanNumerals(i));
