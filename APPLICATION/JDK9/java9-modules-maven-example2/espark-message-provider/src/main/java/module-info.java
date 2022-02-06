86d256e24993c17f64b1f9d89e63742ea59d3b41
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2022-02-05 22:18:51 -0600

    updating

--- APPLICATION/JDK9/java9-modules-maven-example2/espark-message-provider/src/main/java/module-info.java
+++ APPLICATION/JDK9/java9-modules-maven-example2/espark-message-provider/src/main/java/module-info.java
@@ -2,6 +2,8 @@ import com.espark.message.Message;
 
 module espark.message.provider {
     requires espark.message;
+    requires espark.english.message;
+    requires espark.hindi.message;
     uses Message;
     exports com.adarsh.message.provider;
 }
