b3a9f4dad0b7f22dfb39799e60fb0f92022e774a
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2022-11-12 15:00:20 -0600

    adding java 9

---
+++ APPLICATION/JDK9/jlink-basic-example/src/main/ApplicationMain.java
@@ -0,0 +1,9 @@
+package com.espark.adarsh;
+
+
+public class ApplicationMain {
+    public static void main(String[] args) {
+
+        System.out.println("welccome to jlink "+System.getProperty("user.name"));
+    }
+}
